5aa8bbfad80c54573031cfc99f330560a01471f6
dubel <dubel.michal@gmail.com>
2026-01-12 23:35:30 +0100

    comment out

--- install-all.sh
+++ install-all.sh
@@ -2,11 +2,11 @@
 
 # Install all packages in order
 ./install-zsh.sh
-./install-asdf.sh
+# ./install-asdf.sh
 ./install-nodejs.sh
-./install-ruby.sh
-./install-postgresql.sh
-./install-ghostty.sh
+# ./install-ruby.sh
+# ./install-postgresql.sh
+# ./install-ghostty.sh
 ./install-tmux.sh
 ./install-stow.sh
 ./install-dotfiles.sh
